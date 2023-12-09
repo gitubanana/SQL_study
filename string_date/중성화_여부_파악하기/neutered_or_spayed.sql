69e0c87e1a62c09e14a97b83f277cec1f695dff1
gitubanana <ptyolo666@gmail.com>
2023-12-09 20:40:28 +0900

    Created: neutered_or_spayed.sql

---
+++ string_date/중성화_여부_파악하기/neutered_or_spayed.sql
@@ -0,0 +1,8 @@
+SELECT ANIMAL_ID, NAME,
+    CASE
+        WHEN SEX_UPON_INTAKE LIKE BINARY '%Neutered%'
+            OR SEX_UPON_INTAKE LIKE BINARY '%Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS 중성화
+    FROM ANIMAL_INS
+    ORDER BY ANIMAL_ID;
