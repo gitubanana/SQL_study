c724c5f6140368c7110318303b696d0e7e3dd9f2
gitubanana <ptyolo666@gmail.com>
2024-02-15 22:21:45 +0900

    Created: triangle.sql

---
+++ HackerRank/what-type-of-triangle/triangle.sql
@@ -0,0 +1,7 @@
+SELECT CASE
+        WHEN (A + B <= C) OR (B + C <= A) OR (C + A <= B) THEN 'Not A Triangle'
+        WHEN A = B AND B = C THEN 'Equilateral'
+        WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+    FROM TRIANGLES;
