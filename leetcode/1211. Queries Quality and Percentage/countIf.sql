911bb8bd88fdf2b75a7289fa68b813633ee3c75b
Taeyang Park <ptyolo666@gmail.com>
2025-09-04 08:39:14 +0900

    leetcode : 1211 COUNT(IF())

---
+++ leetcode/1211. Queries Quality and Percentage/countIf.sql
@@ -0,0 +1,5 @@
+SELECT query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(COUNT(IF(rating < 3, 1, NULL)) / COUNT(*) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
