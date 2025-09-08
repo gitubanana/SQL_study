bbd55fec03d5632940224602269f3e1d19a27225
Taeyang Park <ptyolo666@gmail.com>
2025-09-08 09:15:22 +0900

    leetcode : 1407 left jion

---
+++ leetcode/1407. Top Travellers/leftJoin.sql
@@ -0,0 +1,6 @@
+SELECT name, IFNULL(SUM(distance), 0) AS travelled_distance
+FROM Users AS A
+LEFT OUTER JOIN Rides AS B
+    ON A.id = B.user_id
+GROUP BY A.id
+ORDER BY travelled_distance DESC, name ASC;
